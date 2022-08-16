656bcee54b4c38542fd11e3e90bc6f6cbff84191
avetik-dev <avetik.martirosyan@esterox.am>
2022-08-17 00:25:07 +0400

    created functionality for auto updating apps

--- Controllers/PostController.php
+++ Controllers/PostController.php
@@ -17,20 +17,39 @@ class PostController
      * @param $key
      * @return false|mixed|Redis|string
      * @throws RedisException
-     * Get value from redis by key
+     * Set value on redis
      */
     public function set_data($data)
     {
-       foreach ($data as $key => $value) {
-           $role = explode('_', $key)[0];
-           $this->redis->set($value, $role);
-//           $this->updateAppUserRole($value, $role);
-       }
-       return ['status' => true];
+        header("Content-Type: application/json");
+
+        if ($data && is_array($data)) {
+            $responseData = [];
+
+            foreach ($data as $key => $value) {
+                $role = explode('_', $key)[0];
+                $res = $this->updateAppUserRole($value, $role);
+                if ($res['status']) {
+                    $this->redis->set($value, $role);
+                    $responseData[$res['app']] = $res['message'];
+                }
+            }
+            echo json_encode($responseData);
+            exit;
+        }
+        echo json_encode(['status' => false, 'message' => 'wrong data']);
+        exit;
     }
 
-    public function updateAppUserRole($app, $role) {
-        $app = explode('&&', $app);
+    /**
+     * @param $dataStr
+     * @param $role
+     * @return array
+     * send application request and update user role
+     */
+    public function updateAppUserRole($dataStr, $role)
+    {
+        $app = explode('&&', $dataStr);
         $url = APPS_UPDATING_URLS[$app[0]];
         $data = [
             'company_id' => $app[1],
@@ -42,9 +61,18 @@ class PostController
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($ch);
-        curl_close ($ch);
+        $response = json_decode(curl_exec($ch));
+        curl_close($ch);
 
-        return $response;
+        if (!file_exists('logs')) {
+            mkdir('logs', 0777, true);
+        }
+
+        $logData = $dataStr . " _____ $role _____ message: " . $response->message . "\n";
+        $log = fopen('logs/connect_to_apps.log', 'a');
+        fwrite($log, $logData);
+        fclose($log);
+
+        return ['app' => $app[0], 'message' => $response->message, 'status' => $response->status];
     }
 }
